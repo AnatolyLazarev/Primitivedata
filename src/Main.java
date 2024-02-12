59cdd728d271c1da9b5475507bbc4db48a00918a
AnatolyLazarev <don.feromon@yandex.ru>
2024-02-12 20:35:40 +0300

    change reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         int price = 13676;
-        int miles = price/20;
+        int miles = price / 20;
         System.out.println("<Бонус:" + miles);
     }
 }
